Refactored code into correct order stitching into columns using length of config file, as well as added a new time elapse function for monitoring length of calculation

src/Main.py
@@ -1,35 +1,32 @@
+import timeElapser
+import locateBedrockInTile
 import cv2 as cv
-import numpy as np
+import json
 import os
-import stitchtiles as st
 
-print('Running script')
+print('Starting script')
+timeElapser.print_elapsed_time()
 dirname = os.path.dirname(__file__)
 dirname = dirname[:-4]
 
-sample = os.path.join(dirname, r'images\-10_-6.jpg')
+## Test code, will remove
+sample = os.path.join(dirname, r'images\-10_-')
 sample = sample.replace(os.sep, '/')
-
-sample2 = os.path.join(dirname, r'images\-10_-5.jpg')
-sample2 = sample2.replace(os.sep, '/')
 
 bedrock = os.path.join(dirname, r'images\bedrock.jpg')
 bedrock = bedrock.replace(os.sep, '/')
 
-img_rgb = cv.imread(sample)
-img2_rgb = cv.imread(sample2)
-template = cv.imread(bedrock)
-h, w = template.shape[:-1]
+with open(dirname+"/src/config.json") as json_data_file:
+    data = json.load(json_data_file)
 
-img_array = []
-img_array.append(img_rgb)
-img_array.append(img2_rgb)
+result = None
+for i in range(data["range"]):
+    sample = sample + str((i + 1)) + ".jpg"
+    if result is None:
+        result = locateBedrockInTile.locateBedrockFirstTime(sample, bedrock)
+    else:
+        result = locateBedrockInTile.locateBedrock(result, sample, bedrock)
+    cv.imwrite('export/result.png', result)
+    sample = sample[:-5]
 
-for img in img_array:
-    res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
-    threshold = .8
-    loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-cv.imwrite('export/result.png', st.merge_images(img_array[0], img_array[1]))
+timeElapser.print_elapsed_time('Ending Script:')

src/stitchtiles.py
@@ -9,12 +9,12 @@ def merge_images(image1, image2):
     (width1, height1) = image1.size
     (width2, height2) = image2.size
 
-    result_width = width1 + width2
-    result_height = max(height1, height2)
+    result_width = max(width1, width2)
+    result_height = height1 + height2
 
     result = Image.new('RGB', (result_width, result_height))
     result.paste(im=image1, box=(0, 0))
-    result.paste(im=image2, box=(width1, 0))
+    result.paste(im=image2, box=(0, height1))
 
     result = np.asarray(result)
     return result